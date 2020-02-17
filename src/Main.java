a68d51d74ce5a7f93900a9e1c8eefa2f562a305e
Olga Norik <norinaoa@mail.ru>
2020-02-17 12:54:15 +0500

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Balance = 2_000_000_000;
+        int Transfer = 500_000_000;
+        int NewBalance = Balance + Transfer;
+        System.out.println(NewBalance);
+    }
+}
